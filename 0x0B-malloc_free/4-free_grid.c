0cd425b02ad946838c2d752ec8e2f0e87538921e
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-08 13:45:34 -0700

    fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,16 +6,15 @@
  * @grid: 2d grid
  * @height: height dimension of grid
  *
- * Return NO data
+ * Return: NO data
  *
  */
 void free_grid(int **grid, int height)
 {
 int X_value;
-for (X_value = 0; X_value < height; i++)
+for (X_value = 0; X_value < height; X_value++)
 {
 free(grid[X_value]);
 }
-free(grid2);
+free(grid);
 }
-
