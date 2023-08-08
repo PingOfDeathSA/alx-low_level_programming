811a76d186d09597972bb02abde70a120686cd86
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-08 13:35:44 -0700

    fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,9 @@ main.h
 
 #ifndef MAIN_H
 #define MAIN_H
-char *_strdup(char *str);
+
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 int _putchar(char c);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
