3eec4b2d9d8d31320db4619ea3577556c1664b9b
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-26 05:20:17 -0700

    sep fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,32 @@
 #include "variadic_functions.h"
-
+#include <stddef.h>
 /**
- * display_strings - outputs a series of strings
- * @separator: the separator used between strings
+ * display_list_strings - outputs a series of list_strings
+ * @separator: the separator used between list_strings
  * @n: count of arguments
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_list_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int sting;
-	char *sting;
+	va_list list_strings;
+	char *str;
+	unsigned int i_ndex;
 
-	va_list sep_list;
-	va_start(sep_list, n);
-	for (sting = 0; sting < n; sting++)
+	va_start(list_strings, n);
+
+	for (i_ndex = 0; i_ndex < n; i_ndex++)
 	{
-		sting = va_arg(sep_list, char *);
-		if (!sting)
-			sting = "(nil)";
-		if (!separator)
-			printf("%s", sting);
-		else if (separator && sting == 0)
-			printf("%s", sting);
+		str = va_arg(list_strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
 		else
-		printf("%s%s", separator, sting);
+			printf("%s", str);
+
+		if (i_ndex != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+
 	printf("\n");
 
-	va_end(sep_list);
+	va_end(list_strings);
 }
