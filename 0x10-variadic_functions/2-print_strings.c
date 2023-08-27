17bb04c577ae92b1f098482750e28adcf8440c67
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-27 00:11:41 -0700

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,27 +1,35 @@
-#include "variadic_functions.h"
+#include <stdarg.h>  
+#include <stdio.h>  
 
 /**
-display_strings - displays a series of strings
-@separator: the separator to be used between strings
-@n: the count of strings to be displayed
-Returns: void
-*/
+ * print_strings - displays a series of strings
+ * @separator: the separator to be used between strings
+ * @n: the count of strings to be displayed
+ *
+ * Return: void
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list listing;
-unsigned int index;
-char *str;
-va_start(listing, n);
-for (index = 0; index < n; index++)
-{
-str = va_arg(listing, char *);
-if (str == NULL)
-printf("(nil)");
-else
-printf("%s", str);
-if (index!= (n - 1) && separator != NULL)
-printf("%s", separator);
-}
-printf("\n");
-va_end(listing);
+    va_list listing;
+    unsigned int index;
+    char *str;
+
+    va_start(listing, n);  
+
+    for (index = 0; index < n; index++)
+    {
+        str = va_arg(listing, char *);  
+
+        if (str == NULL)
+            printf("(nil)");
+        else
+            printf("%s", str);
+
+        if (index != (n - 1) && separator != NULL)
+            printf("%s", separator);
+    }
+
+    printf("\n");
+
+    va_end(listing);  
 }
