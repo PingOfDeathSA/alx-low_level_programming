dfdc496264bf7206114298fb524c2a24ee029e20
PingOfDeathSA <ronaldnt8@gmail.com>
2023-08-28 23:31:19 -0700

    test

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,32 +10,34 @@ Return: Returns 1 if successful, or -1 if the operation is unsuccessful.
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *temporary = *head;
-listint_t *previous = NULL;
-unsigned int position = 0;
+listint_t *temporary = *head; 
+listint_t *previous = NULL; 
+unsigned int position = 0; 
 
-if (*head == NULL)
-    return -1;
 
-if (index == 0)
-{
-    *head = (*head)->next;
-    free(temporary);
-    return 1;
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temporary);
+		return (1);
+	}
+
+	while (position < index - 1)
+	{
+		if (!temporary || !(temporary->next))
+			return (-1);
+		temporary = temporary->next;
+		position++;
+	}
+
+
+	previous = temporary->next;
+	temporary->next = previous->next;
+	free(previous);
+
+	return (1);
 }
 
-while (position < index - 1)
-{
-    if (!temporary || !(temporary->next))
-        return -1;
-    previous = temporary;
-    temporary = temporary->next;
-    position++;
-}
-
-listint_t *onecurrent1 = temporary->next;
-previous->next = onecurrent1;
-free(temporary);
-
-return 1;
-}
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,3 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
 /**
@@ -7,25 +6,70 @@ display_safe_listint - securely prints a linked list
 @head: list of type listint_t to be displayed
 Return: count of nodes in the list
 */
-size_t print_listint_safe(const listint_t *head)
-{
- size_t count = 0;
-ptrdiff_t distance;
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-while (head)
-{
-    distance = (ptrdiff_t)(head - head->next);
-    count++;
-    printf("[%p] %d\n", (void *)head, head->n);
-    
-    if (distance > 0)
-        head = head->next;
-    else
-    {
-        printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-        break;
+size_t looped_listint_len(const listint_t *head);
+
+size_t looped_listint_len(const listint_t *head) {
+    const listint_t *ptr1, *ptr2;
+    size_t count = 0;
+
+    if (head == NULL || head->next == NULL)
+        return count;
+
+    ptr1 = head->next;
+    ptr2 = (head->next)->next;
+
+    while (ptr2 && ptr2->next) {
+        if (ptr1 == ptr2) {
+            ptr1 = head;
+            while (ptr1 != ptr2) {
+                count++;
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+            }
+
+            ptr1 = ptr1->next;
+            while (ptr1 != ptr2) {
+                count++;
+                ptr1 = ptr1->next;
+            }
+
+            return count;
+        }
+
+        ptr1 = ptr1->next;
+        ptr2 = (ptr2->next)->next;
     }
+
+    return count;
 }
 
-return count;
+size_t print_listint_safe(const listint_t *head) {
+    size_t totalNodes = 0;
+
+    totalNodes = looped_listint_len(head);
+
+    if (totalNodes == 0) {
+        while (head != NULL) {
+            printf("[%p] %d\n", (void *)head, head->n);
+            head = head->next;
+            totalNodes++;
+        }
+    } else {
+        size_t currentIndex;
+        for (currentIndex = 0; currentIndex < totalNodes; currentIndex++) {
+            printf("[%p] %d\n", (void *)head, head->n);
+            head = head->next;
+        }
+
+        printf("-> [%p] %d\n", (void *)head, head->n);
+    }
+
+    return totalNodes;
 }
+
+
