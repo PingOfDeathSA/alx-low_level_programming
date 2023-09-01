7787c6ca2a719d2e372b7d331bfea3e45e08d956
PingOfDeathSA <ronaldnt8@gmail.com>
2023-09-01 05:12:14 -0700

    alx

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the machine.
+ *
+ * Return: 0 for big endian, 1 for little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	return (*((char *)&i) == 1);
+}
