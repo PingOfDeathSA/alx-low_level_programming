b393b8c2bc0476947175048cd9122b9a90db7a4e
PingOfDeathSA <ronaldnt8@gmail.com>
2023-09-01 04:57:20 -0700

    alx

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,36 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_binary - Displays the binary representation of a decimal number.
  * @n: The decimal number to be displayed in binary.
  */
-
 void print_binary(unsigned long int n)
- {
-    int shift;
-    int flag = 0; 
+{
+	int indexing, value_ount = 0;
+	unsigned long int temp;
 
-    if (n == 0) {
-        putchar('0');
-        return;
-    }
+	for (indexing = 63; indexing >= 0; indexing--)
+	{
+		temp = n >> indexing;
 
-    for (shift = sizeof(n) * 8 - 1; shift >= 0; shift--)
-     {
-        if ((n >> shift) & 1) { 
-            putchar('1');
-            flag = 1;
-        } else if (flag) {
-            putchar('0');
-        }
-    }
-}
-
-int main() {
-    unsigned long int decimal = 42; 
-    printf("Binary representation of %lu: ", decimal);
-    print_binary(decimal);
-    putchar('\n');
-    
-    return 0;
+		if (temp & 1)
+		{
+			_putchar('1');
+			value_ount++;
+		}
+		else if (value_ount)
+			_putchar('0');
+	}
+	if (!value_ount)
+		_putchar('0');
 }
