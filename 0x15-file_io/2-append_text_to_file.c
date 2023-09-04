ff25e89e2680cb39572e0bae43a954cc9b9642d3
PingOfDeathSA <ronaldnt8@gmail.com>
2023-09-04 11:40:24 -0700

    test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,30 +15,24 @@ int append_text_to_file(const char *filename, char *text_content)
 {
     int file_descriptor, bytes_written, text_length = 0;
 
-    // Check if the filename is NULL; return -1 if it is.
+
     if (filename == NULL)
         return (-1);
 
-    // Calculate the length of the text_content string.
     if (text_content != NULL) {
         while (text_content[text_length] != '\0')
             text_length++;
-    }
 
-    // Open the file for writing in append mode.
     file_descriptor = open(filename, O_WRONLY | O_APPEND);
 
-    // Write the text_content to the end of the file and store the number of bytes written in 'bytes_written'.
-    bytes_written = write(file_descriptor, text_content, text_length);
+   bytes_written = write(file_descriptor, text_content, text_length);
 
-    // Check if either the file descriptor (file_descriptor) or the write operation (bytes_written) failed.
-    // If so, return -1.
+
     if (file_descriptor == -1 || bytes_written == -1)
         return (-1);
 
-    // Close the file descriptor and return 1 to indicate success.
     close(file_descriptor);
 
     return (1);
 }
-
+}
