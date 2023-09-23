06f9e439a65bdd54cf0c4715ca788f46f77f0636
PingOfDeathSA <ronaldnt8@gmail.com>
2023-09-23 06:20:40 -0700

    fixing errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * Function: read_textfile
  * Purpose: Reads the content of a text file and outputs it to the standard output.
- * 
  * @param filename: The name of the text file to be read.
  * @param letters: The number of characters to be read.
  * @return: The actual number of bytes read and printed, denoted as 'w'.
- *         Returns 0 in case of function failure or if the filename is NULL.
+ * Returns 0 in case of function failure or if the filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *bffer;
-	ssize_t fileD;
-	ssize_t x;
-	ssize_t y;
-
-	fileD = open(filename, O_RDONLY);
-	if (fileD == -1)
-		return (0);
-	bffer = malloc(sizeof(char) * letters);
-	y = read(fileD, bffer, letters);
-	x = write(STDOUT_FILENO, bffer, y);
-
-	free(bffer);
-	close(fileD);
-	return (x);
+int file_d;
+ssize_t num_of_reads, num_of_writes;
+char *b_uffer;
+if (!filename)
+return (0);
+file_d = open(filename, O_RDONLY);
+if (file_d == -1)
+return (0);
+b_uffer = malloc(sizeof(char) * (letters));
+if (!b_uffer)
+return (0);
+num_of_reads = read(file_d, b_uffer, letters);
+num_of_writes = write(STDOUT_FILENO, b_uffer, num_of_reads);
+close(file_d);
+free(b_uffer);
+return (num_of_writes);
 }
+
